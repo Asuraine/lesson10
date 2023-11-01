d330294944cb51b7c7d747d0dd0523d4fab62cbb
Elena <elena.lichman@mail.ru>
2023-11-01 18:22:54 +0300

    lesson10 finish

---
+++ src/main/java/ru/sbgeu/lichman/basic/box/MainApl.java
@@ -0,0 +1,22 @@
+package ru.sbgeu.lichman.basic.box;
+
+public class MainApl {
+    public static void main(String[] args) {
+
+        Box box = new Box("Красный");
+        box.info();
+        box.add("Чашка");
+        box.info();
+        box.clear("Чашка");
+        box.info();
+        box.openBoxes();
+        box.add("Чашка");
+        box.closedBoxes();
+        box.add("Чашка");
+        box.info();
+        box.setColor("Черный");
+        box.info();
+
+
+    }
+}
